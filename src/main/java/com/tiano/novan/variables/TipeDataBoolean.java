6b18c297620895d2f16b8090957fa89dbfe58c2c
novan09082000 <josenoutinho@gmail.com>
2021-12-10 22:24:41 +0700

    buat variabel boolean

---
+++ src/main/java/com/tiano/novan/variables/TipeDataBoolean.java
@@ -0,0 +1,12 @@
+package com.tiano.novan.variables;
+
+public class TipeDataBoolean {
+    
+    public static void main(String[] args) {
+        
+        Boolean isAuthenticate = false;
+        System.out.println("apakah anda telah login? " + (isAuthenticate ? "Sudah" : "Belum"));
+
+    }
+
+}
